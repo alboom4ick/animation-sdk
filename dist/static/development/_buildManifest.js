00dfb10291ab1d5f117afb851d5e28c252564795
alboom4ick <alibekomirzak400@gmail.com>
2025-06-25 15:07:04 +0500

    deleted everything

--- dist/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {__rewrites:{afterFiles:[],beforeFiles:[{has:a,source:"\u002Fmeet\u002Fanimation-next-js\u002F_next\u002F:path+",destination:"\u002Fanimation-next-js\u002F_next\u002F:path+"}],fallback:[]},__routerFilterStatic:a,__routerFilterDynamic:a,sortedPages:["\u002F_app"]}}(void 0));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
